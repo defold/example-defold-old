cec341f84b83e6b568e39086c20623f96e4de935
Mikael Säker <mikael@sicher.org>
2014-06-11 14:19:03 +0200

    - Behaviors example. - Working on how to deal with hot reloading and modules (in modules_example)

--- behaviors_example/flying.lua
+++ behaviors_example/flying.lua
@@ -1,13 +1,6 @@
-print("FLYING!")
-
--- The table “flying” contains the module.
+-- This is a module implementation of a simple flying behavior.
 local flying = {}
 
--- We use a table to store original positions. Note that the module becomes part
--- of all game objects' shared Lua context so we can't just store the origin in
--- a variable straight - it would be overwritten if more than one GO used this module.
--- Beware that this internal state could be a potential source of bugs since it
--- introduces coupling between the users of the module.
 flying.origins = {}
 
 -- Crudely fly to a random position at "radius" distance from the original position
@@ -16,7 +9,6 @@ flying.origins = {}
 function flying.fly_randomly(radius)
 	-- Radius is 100 unless specified
 	radius = radius or 100
-	-- We need current object id to index the original position. Can't use "."
 	local go_id = go.get_id()
 
 	-- Store origin position if it's not already stored.
@@ -29,8 +21,6 @@ function flying.fly_randomly(radius)
 	local rand_radius = math.random(radius)
 	local offset = vmath.rotate(vmath.quat_rotation_z(rand_angle), vmath.vector3(rand_radius, 0, 0))
 	local rand_pos = flying.origins[go_id] + offset
-	-- Set a random duration scaled against the radius to prevent
-	-- too fast animation.
 	local rand_duration = math.random(radius) / 100 + (radius / 200)
 
 	-- Animate, then send a message back when completed
