01dec0827d5fd942c2685d5b93e9fd58b207cd8e
Mikael Säker <mikael@sicher.org>
2014-07-20 22:05:46 +0200

    Fixed main menu collection proxy path errors. Physics example now has trigger for death.

--- physics_example/player.go
+++ physics_example/player.go
@@ -16,7 +16,7 @@ components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.0\nrestitution: 0.9\ngroup: \"player\"\nmask: \"world\"\nmask: \"locks\"\nmask: \"balls\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 3\n  }\n  data: 20.0\n  data: 44.0\n  data: 10.0\n}\n"
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.0\nrestitution: 0.9\ngroup: \"player\"\nmask: \"world\"\nmask: \"locks\"\nmask: \"balls\"\nmask: \"triggers\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 3\n  }\n  data: 20.0\n  data: 44.0\n  data: 10.0\n}\n"
   position {
     x: 0.0
     y: 0.0
