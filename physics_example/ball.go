6860b158b8ff46bc25d579046d9c24b4eb3797c1
Mikael Säker <mikael@sicher.org>
2014-08-18 12:27:16 +0200

    Fixed bug in physics example. Added some documentation as well.
    Empty animation example collection added.

--- physics_example/ball.go
+++ physics_example/ball.go
@@ -1,7 +1,7 @@
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_DYNAMIC\nmass: 0.1\nfriction: 0.1\nrestitution: 0.7\ngroup: \"balls\"\nmask: \"player\"\nmask: \"world\"\nmask: \"locks\"\nmask: \"balls\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: -0.13082999\n      y: -3.9248996\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 28.13992\n}\n"
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_DYNAMIC\nmass: 0.1\nfriction: 0.1\nrestitution: 0.7\ngroup: \"balls\"\nmask: \"player\"\nmask: \"world\"\nmask: \"locks\"\nmask: \"balls\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: -0.13082999\n      y: -3.9248996\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 28.13992\n}\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
   position {
     x: 0.0
     y: 0.0
