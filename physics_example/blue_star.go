52b47f2af360fefaac043af4587a4a202c25ff58
Mikael Säker <mikael@sicher.org>
2014-09-30 09:18:34 +0200

    Updated physics example with objects (stars) that has the Box2D damping properties set.

---
+++ physics_example/blue_star.go
@@ -0,0 +1,32 @@
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_DYNAMIC\nmass: 3.0\nfriction: 0.3\nrestitution: 0.7\ngroup: \"balls\"\nmask: \"player\"\nmask: \"world\"\nmask: \"locks\"\nmask: \"balls\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: -0.13082999\n      y: -3.9248996\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 28.13992\n}\nlinear_damping: 0.0\nangular_damping: 0.9\nlocked_rotation: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/physics_example/sprites.atlas\"\ndefault_animation: \"blue_star\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- physics_example/level.go
+++ physics_example/level.go
@@ -16,7 +16,7 @@ components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"/physics_example/level.tilemap\"\ntype: COLLISION_OBJECT_TYPE_STATIC\nmass: 0.0\nfriction: 0.5\nrestitution: 0.5\ngroup: \"\"\nmask: \"player\"\nmask: \"balls\"\n"
+  data: "collision_shape: \"/physics_example/level.tilemap\"\ntype: COLLISION_OBJECT_TYPE_STATIC\nmass: 0.0\nfriction: 0.5\nrestitution: 0.5\ngroup: \"\"\nmask: \"player\"\nmask: \"balls\"\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
   position {
     x: 0.0
     y: 0.0
