bbc039e8904499ff9641e6fb964523a46365e4f0
Mikael Säker <mikael@sicher.org>
2014-10-20 15:00:40 +0200

    Expanded the Magic example with an additional spell that changes the color of all spawned butterflies.

--- magic_example/butterfly.go
+++ magic_example/butterfly.go
@@ -14,9 +14,25 @@ components {
   }
 }
 embedded_components {
+  id: "alt_sprite"
+  type: "sprite"
+  data: "tile_set: \"/magic_example/magic_sprites.atlas\"\ndefault_animation: \"butterfly_blue\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/magic_example/magic_sprites.atlas\"\ndefault_animation: \"butterfly_orange\"\n"
+  data: "tile_set: \"/magic_example/magic_sprites.atlas\"\ndefault_animation: \"butterfly_orange\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
   position {
     x: 0.0
     y: 0.0
