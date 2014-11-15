3e85d24a96bc89eb1b7bfc1e024acdc8ea47edaa
Mikael Säker <mikael@sicher.org>
2014-11-15 15:27:01 +0100

    Fireworks example with sound gating and a function that keeps track of particles and tries to spawn fireworks close to the max number of allowed particles.

---
+++ fireworks_example/fireworks.go
@@ -0,0 +1,45 @@
+components {
+  id: "fireworksfx_big"
+  component: "/fireworks_example/fireworks_big.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "fireworksfx_small"
+  component: "/fireworks_example/fireworks_small.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/fireworks_example/fireworks.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
