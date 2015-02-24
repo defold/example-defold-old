9336b9ac614d9f6d2d85e2ee9ba3996ca6fdc88f
Mikael Säker <mikael@sicher.org>
2015-02-24 15:26:40 +0100

    Collection spawning example.

---
+++ collectionfactory_example/probe.go
@@ -0,0 +1,31 @@
+components {
+  id: "script"
+  component: "/collectionfactory_example/probe.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/collectionfactory_example/sprites.atlas\"\ndefault_animation: \"green_probe\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
