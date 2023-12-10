2cd5e985d4e2ade56e7c76eecad2f7c925f1a2ef
AGulev <me@agulev.com>
2023-12-10 11:01:45 +0100

    make it possible to build in 1.6.2

--- magic_example/wand.go
+++ magic_example/wand.go
@@ -18,6 +18,7 @@ embedded_components {
   type: "factory"
   data: "prototype: \"/magic_example/magic.go\"\n"
   "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
   ""
   position {
     x: 0.0
